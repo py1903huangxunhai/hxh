8bc0bfb173f6d7d6c826b868bb51c630298437b2
hxh137033 <599954453@qq.com>
2019-06-13 21:43:40 +0800

    完善文章、分类、标签、评论表 时间：0613  21:40

--- demo3/blog/models.py
+++ demo3/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Tag(models.Model):
     """
     标签表：与文章表存在多对多
@@ -28,6 +29,7 @@ class Article(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
     create_time = models.DateTimeField(auto_now=True)
     update_time = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect,reverse
 from django.views.generic import View,ListView,DetailView
+from .models import *
+from comments.forms import CommentForms
 # Create your views here.
 
 class IndexView(View):
+    """
+    文章列表页视图类
+    """
     def get(self,req):
-        return render(req,"blog/index.html")
+        """
+        重写get请求
+        :param req:
+        :return:
+        """
+        articles =Article.objects.all()
+        return render(req,"blog/index.html",locals())
+
+# class IndexView(View):
+#     def get(self,req):
+#         return render(req,"blog/index.html")
 
 class SingleView(View):
+    """
+    文章详情页视图
+    """
     def get(self,req,id):
-        return render(req,"blog/single.html")
+        """
+        重写get请求
+        :param req:
+        :param id:  文章id
+        :return:
+        """
+        article = Article.objects.get(pk=id)
+        cf=CommentForms()
+        return render(req,"blog/single.html",locals())
+
+    def post(self,req,id):
+        cf=CommentForms(req.POST)
+        if cf.is_valid():
+            # 暂时不保存数据库
+            res=cf.save(commit=False)
+            res.article=get_object_or_404(Article,pk=id)
+            # 给评论表中的文章赋值后才可以保存
+            res.save()
+            return redirect(reverse('blog:single',args=(id,)))

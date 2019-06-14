02ad82c54e3ab04770bc628bb0ca9fb939eb5f3d
hxh137033 <599954453@qq.com>
2019-06-14 14:40:01 +0800

    完善文章 归档 分类 标签云 时间：0614  13:00

---
+++ demo3/blog/templatetags/my_fun.py
@@ -0,0 +1,32 @@
+"""
+自定义标签
+
+"""
+
+from django.template import Library
+from ..models import Article,Category,Tag
+register = Library()
+
+@register.simple_tag
+def getlatestarticles(num=3):
+    """
+    得到最新的文章
+    :param num:
+    :return:
+    """
+    return Article.objects.all().order_by('-create_time')[:num]
+
+
+@register.simple_tag
+def getarchives():
+    # result = Article.objects.dates("create_time","month")
+    # return result
+    return Article.objects.dates("create_time","month")
+
+@register.simple_tag
+def getcategorys():
+    return Category.objects.all()
+
+@register.simple_tag
+def gettags():
+    return Tag.objects.all()
--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -15,11 +15,9 @@ class IndexView(View):
         :return:
         """
         articles =Article.objects.all()
+        # latestarticles = articles.order_by("-create_time")[:3]
         return render(req,"blog/index.html",locals())
 
-# class IndexView(View):
-#     def get(self,req):
-#         return render(req,"blog/index.html")
 
 class SingleView(View):
     """
@@ -34,6 +32,9 @@ class SingleView(View):
         """
         article = Article.objects.get(pk=id)
         cf=CommentForms()
+
+        # latestarticles = Article.objects.all().order_by("-create_time")[:3]
+
         return render(req,"blog/single.html",locals())
 
     def post(self,req,id):
@@ -45,3 +46,21 @@ class SingleView(View):
             # 给评论表中的文章赋值后才可以保存
             res.save()
             return redirect(reverse('blog:single',args=(id,)))
+
+
+class ArchieveView(View):
+    def get(self,req,year,month):
+        articles = Article.objects.filter(create_time__year = year, create_time__month = month)
+        return render(req, "blog/index.html", locals())
+
+class CategoryView(View):
+    def get(self,req,id):
+        category = get_object_or_404(Category,pk=id)
+        articles = category.article_set.all()
+        return render(req, "blog/index.html", locals())
+
+class TagView(View):
+    def get(self,req,id):
+        tag = get_object_or_404(Tag,pk=id)
+        articles = tag.article_set.all()
+        return render(req, "blog/index.html", locals())

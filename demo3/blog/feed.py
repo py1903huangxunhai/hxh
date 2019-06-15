2f72badffd4fe987893c302d80fcb39babc60b7b
hxh137033 <599954453@qq.com>
2019-06-15 11:20:35 +0800

    自定义标签 分页 markdown语法 时间：0615  11:20

---
+++ demo3/blog/feed.py
@@ -0,0 +1,23 @@
+"""
+使用Django自带模块将最新更新包装成XML
+"""
+
+from django.contrib.syndication.views import Feed
+from .models import Article
+
+class ArticleFeed(Feed):
+    title = "HXH的个人博客"
+    description = "给大家介绍一些Django的开发知识点"
+    link = "/"
+
+    def items(self):
+        return Article.objects.all().order_by("-create_time")[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body[:30]
+
+    def item_link(self, item):
+        return "/single/%s" % (item.id)
--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,get_object_or_404,redirect,reverse
 from django.views.generic import View,ListView,DetailView
 from .models import *
 from comments.forms import CommentForms
+from comments.models import Comment
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+import markdown
 # Create your views here.
 
 class IndexView(View):
@@ -14,9 +18,31 @@ class IndexView(View):
         :param req:
         :return:
         """
-        articles =Article.objects.all()
+        articles = Article.objects.all()
         # latestarticles = articles.order_by("-create_time")[:3]
-        return render(req,"blog/index.html",locals())
+
+        # # 得到分页
+        # paginator = Paginator(articles,2)
+        # print(paginator.count)
+        # print(paginator.object_list)
+        # print(paginator.num_pages)
+        # print(paginator.page_range)
+        # # 得到页面
+        # page = paginator.get_page(2)
+        # print(page.object_list)
+        # # 由页面得到分页    分页可以得到页面  页面可以得到分页
+        # print(page.paginator)
+        # print(page.number)
+        # print(paginator is page.paginator)
+
+
+        paginator = Paginator(articles,1)
+        pagenum = req.GET.get("page")
+        pagenum = 1 if pagenum == None else pagenum
+        page = paginator.get_page(pagenum)
+        page.path = "/"
+        return render(req,"blog/index.html",{"page":page})
+        # return render(req,"blog/index.html",locals())
 
 
 class SingleView(View):
@@ -30,11 +56,23 @@ class SingleView(View):
         :param id:  文章id
         :return:
         """
-        article = Article.objects.get(pk=id)
-        cf=CommentForms()
+        # article = Article.objects.get(pk=id)
+        article = get_object_or_404(Article, pk=id)
 
+        # 1获取markdown实例
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        # 2 使用markdown实例渲染指定字段
+        article.body = md.convert(article.body)
+        # 3 将md的目录对象赋予 article
+        article.toc = md.toc
+
+        # 向详情页面传递一个评论表单
+        cf = CommentForms()
         # latestarticles = Article.objects.all().order_by("-create_time")[:3]
-
         return render(req,"blog/single.html",locals())
 
     def post(self,req,id):
@@ -51,16 +89,32 @@ class SingleView(View):
 class ArchieveView(View):
     def get(self,req,year,month):
         articles = Article.objects.filter(create_time__year = year, create_time__month = month)
-        return render(req, "blog/index.html", locals())
+
+        paginator = Paginator(articles,1)
+        pagenum = req.GET.get("page")
+        pagenum = 1 if pagenum == None else pagenum
+        page = paginator.get_page(pagenum)
+        page.path = "/archives/%s/%s/" % (year,month)
+        return render(req, "blog/index.html", {"page":page})
 
 class CategoryView(View):
     def get(self,req,id):
         category = get_object_or_404(Category,pk=id)
         articles = category.article_set.all()
+        paginator = Paginator(articles, 1)
+        pagenum = req.GET.get("page")
+        pagenum = 1 if pagenum == None else pagenum
+        page = paginator.get_page(pagenum)
+        page.path = "/category/%s/" % (id,)
         return render(req, "blog/index.html", locals())
 
 class TagView(View):
     def get(self,req,id):
         tag = get_object_or_404(Tag,pk=id)
         articles = tag.article_set.all()
+        paginator = Paginator(articles, 1)
+        pagenum = req.GET.get("page")
+        pagenum = 1 if pagenum == None else pagenum
+        page = paginator.get_page(pagenum)
+        page.path = "/tags/%s/" % (id,)
         return render(req, "blog/index.html", locals())
